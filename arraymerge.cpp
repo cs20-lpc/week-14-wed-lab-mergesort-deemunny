d5d01537069f65b82a0015fd8f0edaf1fcc73102
Student <Student@laspositascollege.edu>
2026-04-27 21:32:11 -0700

    moving PC

--- arraymerge.cpp
+++ arraymerge.cpp
@@ -5,8 +5,44 @@ void merge(int arr[], int left, int mid, int right)
 {
     // TODO:
     // Create temporary arrays
+    int leftSize = mid - left + 1;
+    int rightSize = right - mid;
+
+    int* arrLeft = new int[leftSize];
+    int* arrRight = new int[rightSize];
     // Copy data
+
+    for (int l = 0; l < leftSize; ++l) arrLeft[l] = arr[left + l];
+    for (int r = 0; r < rightSize; ++r) arrRight[r] = arr[mid + 1 + r];
     // Merge sorted halves back into arr
+
+    int i = 0, j = 0, k = left;
+    while (i < leftSize && j < rightSize) {
+        if (arrLeft[i] <= arrRight[j]) {
+            arr[k] = arrLeft[i];
+            ++i;
+        } else {
+            arr[k] = arrRight[j]; 
+            ++j;
+        }
+        ++k;
+    }
+
+    while (i < leftSize) {
+        arr[k] = arrLeft[i];
+        ++i;
+        ++k;        
+    }
+
+    while (j < rightSize) {
+        arr[k] = arrRight[i];
+        ++j;
+        ++k;
+    }
+
+    delete[] arrLeft;
+    delete[] arrRight;
+    
 }
 
 void mergeSort(int arr[], int left, int right)
@@ -15,9 +51,13 @@ void mergeSort(int arr[], int left, int right)
     {
         // TODO:
         // Find middle
+        int mid = left + (right - left) / 2;
         // Recursively sort left half
+        mergeSort(arr, left, mid);
         // Recursively sort right half
+        mergeSort(arr, mid + 1, right);
         // Merge both halves
+        merge(arr, left, mid, right);
     }
 }
 
